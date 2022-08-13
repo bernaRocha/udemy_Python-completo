32fdd582352e4e9ed4e0c367596f53b8bed8829c
Bernardo <mrochabernardo@gmail.com>
2022-08-13 11:52:53 -0300

    Aula de aritmética

---
+++ aula_05-operadores-aritmeticos/aula_05.py
@@ -0,0 +1,7 @@
+"""
++, -, *, /, //, **, %, ()
+"""
+
+print('Divisão inteira 10 // 3 = ', 10 // 3) #  3
+print('Repetição de strings ', '-' * 10 ) #  ----------
+print('Potenciação 2 ** 10 ',2 ** 10)
