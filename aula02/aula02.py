5fb9cdef9f37cd6ff10005268d303e2ff126fa12
Bernardo <mrochabernardo@gmail.com>
2022-08-13 10:54:24 -0300

    Aula de string e refatoração do primeiro arquivo

--- aula02/aula02.py
+++ aula02/aula02.py
@@ -1,1 +1,3 @@
 print("Olá da aula 2")
+
+print('Bernardo', 'Rocha', sep='-') # Por padrão há um espaço na função print
---
+++ aula_03/aula_03.py
@@ -0,0 +1,7 @@
+# Tudo é Python é um objeto
+
+print('Com aspas simples')
+print("Com aspas duplas")
+print("Com duplas e 'simples'")  # preferível
+print('Com simples e "duplas"')  # preferível
+print("Com duplas e \"duplas\"") # confuso
