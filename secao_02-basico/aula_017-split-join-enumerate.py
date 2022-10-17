4c5e0b4d12ffff0f7971fb8c3e3e591994582b03
Bernardo <mrochabernardo@gmail.com>
2022-10-17 15:26:01 -0300

    split join

---
+++ secao_02-basico/aula_017-split-join-enumerate.py
@@ -0,0 +1,30 @@
+from os import system
+system('clear')
+
+string = "O Brasil é o país da urna eletrônica, delícia de urna"
+
+lista = string.split(' ')
+lista2 = string.split(',')
+
+print(lista)  # ['O', 'Brasil', 'é', 'o', 'país', 'da', 'urna', 'eletrônica,', 'delícia', 'de', 'urna']
+print(lista2) # ['O Brasil é o país da urna eletrônica', ' delícia de urna']
+
+termo = ''
+contador = 0
+
+for palavra in lista:
+    qtd_vezes = lista.count(palavra)
+    
+    if qtd_vezes > contador:
+        contador = qtd_vezes    
+        termo = palavra
+
+print(f'A palavra "{palavra}" apareceu {contador}x na frase') # A palavra "urna" apareceu 2x na frase
+
+outra_string = "Estou cansando para palavrão grande"
+lista2 = outra_string.split(' ')
+outra_string_02 = ',,'.join(lista2)
+
+print(outra_string)    # Estou cansando para palavrão grande
+print(lista2)          # ['Estou', 'cansando', 'para', 'palavrão', 'grande']
+print(outra_string_02) # Estou,,cansando,,para,,palavrão,,grande
